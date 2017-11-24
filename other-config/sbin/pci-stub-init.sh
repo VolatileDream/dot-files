e4fffa46e36d3b394a08ad59a8492dfa079be871
Gianni Gambetti <gianni@gambetti.ca>
2017-11-24 12:04:27 -0800

    pci-stub init script
    an init script for pci-stub to capture the second graphics card on litigo
    and enable them to be used in a guest VM.

---
+++ other-config/sbin/pci-stub-init.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# List of devices to override with pci-stub, this allows them to be claimed
+# by a guest VM, and avoid issues with being initialized by the host.
+DEVICES=(
+    # _SECOND_ nvidia GPU (and audio subcomponent)
+    # address can be found using "lspci -nnvvv"
+    # Can also be found using nvidia-settings (look for the GPU info & bus id)
+    "0000:03:00.0" "0000:03:00.1"
+)
+
+for DEV in "${DEVICES[@]}" ; do
+    echo "pci-stub" > "/sys/bus/pci/devices/${DEV}/driver_override"
+done
+
+# This is the install script for pci-stub, we need to modprobe the module
+# without recursively invoking ourselves again.
+modprobe -i pci-stub
