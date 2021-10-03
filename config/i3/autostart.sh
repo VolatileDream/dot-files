203410ea06d996c4ce5f3b54f3406cecb8661cca
Gianni Gambetti <gianni@gambetti.ca>
2021-10-04 01:45:35 +0200

    Refactor i3 setup for HOST mappings
    Changes the organization of the i3 files to take advantage of the
    HOST- mapping functionality of dot-deploy. Also provides a desktop
    specific i3 autostart file which handles monitors, and a default
    which assumes that whatever the existing workspace layout is fine.

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -1,27 +1,14 @@
 #!/usr/bin/env bash
 
 i3_setup() {
-  # Setup all the screens.
-  # Remember that workspaces 1 to n exist, where n is the number of monitors.
-  # Swap them to n + 99 so we can remap the workspaces
-  read -d '' i3_reset << EOF
-  workspace 1;
-  workspace 100;
-  workspace 2;
-  workspace 101;
-EOF
-
-  # Go to specific monitors and swap around the workspaces.
   read -d '' i3_set << EOF
-  focus output DP-4;
-    workspace 1; append_layout $HOME/.config/i3/term-disks.json;
-    workspace 2; append_layout $HOME/.config/i3/firefox.json;
-  focus output HDMI-0;
-    workspace 5; append_layout $HOME/.config/i3/discord.json;
-    workspace 6; append_layout $HOME/.config/i3/steam.json;
+    workspace 1; append_layout $HOME/.config/i3/layouts/term-disks.json;
+    workspace 2; append_layout $HOME/.config/i3/layouts/firefox.json;
+    workspace 5; append_layout $HOME/.config/i3/layouts/discord.json;
+    workspace 6; append_layout $HOME/.config/i3/layouts/steam.json;
 EOF
 
-  i3-msg "${i3_reset}" && i3-msg "${i3_set}"
+  i3-msg "${i3_set}"
 }
 
 start_programs() {
@@ -31,7 +18,7 @@ start_programs() {
   gnome-disks &
   discord &
   run_keybase &
-  # steam &
+  # steam-wrapper &
 }
 
 i3_setup && start_programs && i3-msg "workspace 1;"
